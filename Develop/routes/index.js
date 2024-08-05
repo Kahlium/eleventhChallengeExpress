c63607be35bee796151f6db8fa56834748aa327b
Kahlium <kahligtz@gmail.com>
2024-08-05 17:29:02 -0600

    created post and get for notes, server for testing isnt working

---
+++ Develop/routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// Import our modular routers
+
+
+module.exports = router;
---
+++ Develop/routes/notes.js
@@ -0,0 +1,23 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+
+// GET all notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
+});
+
+// POST note
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        }
+    }
+});
+
+
+module.exports = notes;
